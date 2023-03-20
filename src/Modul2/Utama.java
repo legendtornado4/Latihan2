43e4725108a286c2b198a922caf36282eb14311c
OJAN <legendtornado4@gmail.com>
2023-03-20 17:47:22 +0700

    Commit

---
+++ src/Modul2/Utama.java
@@ -0,0 +1,35 @@
+package Modul2;
+import java.util.*;
+
+public class Utama {public static void main(String[] args) {
+
+    ArrayList<String> hewan = new ArrayList<>();
+
+
+    hewan.add("sapi");
+    hewan.add("kelinci");
+    hewan.add("kambing");
+    hewan.add("unta");
+    hewan.add("domba");
+
+
+    ArrayList<String> deletehewan = new ArrayList<>();
+
+
+    deletehewan.add("kelinci");
+    deletehewan.add("kambing");
+    deletehewan.add("unta");
+
+
+    System.out.println("Hewan: " + hewan);
+    System.out.println("Hewan yang dihapus: \n" + deletehewan);
+    hewan.removeAll(deletehewan);
+
+
+    System.out.println("Output Hewan: \n" + hewan);
+
+
+
+}
+}
+
