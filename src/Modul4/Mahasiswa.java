ce0bda043a1ba7e7a86350f7bd9d95720a2afd1c
OJAN <legendtornado4@gmail.com>
2023-04-10 13:59:23 +0700

    Commit

---
+++ src/Modul4/Mahasiswa.java
@@ -0,0 +1,45 @@
+package Modul4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Mahasiswa {
+        String nama, kelas, matkulPraktikum;
+        int nim;
+
+        public Mahasiswa (String nm, String kl, String mat, int ni){
+
+    nama = nm;
+    kelas = kl;
+    matkulPraktikum = mat;
+    nim = ni;
+
+}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        HashMap<String, Mahasiswa> mhs = new HashMap<>();
+        String input;
+        Mahasiswa data;
+
+        mhs.put("1", new Mahasiswa ( "Putri","3H","Struktur Data", 2828888));
+        mhs.put("2", new Mahasiswa (  "Agus","3A","Matematika", 2828012));
+        mhs.put("3", new Mahasiswa("Arro", "30", "Penrouranan", 2820017));
+
+        System.out.println("Masukkan ID: ");
+
+        input = in.nextLine();
+
+        data = mhs.get(input);
+
+        if (data != null) {
+
+            System.out.println("Data Mahasiswa : " + data.nama + ", kelas : " +
+
+                    data.kelas + ", mata kuliah praktikum : " + data.matkulPraktikum +
+                    ", nim: " + data.nim);
+        }
+
+        }
+}
